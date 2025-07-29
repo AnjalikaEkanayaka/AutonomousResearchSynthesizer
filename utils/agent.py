c468956236a65335f4ae60e3d90f8c219881d36c
unknown <anjalika.jke@gmail.com>
2025-07-29 21:32:20 +0530

    Added memory to the agent

--- utils/agent.py
+++ utils/agent.py
@@ -1,3 +1,4 @@
+from langchain.memory import ConversationBufferMemory
 from langchain.agents import initialize_agent
 from langchain.agents.agent_types import AgentType
 from langchain_google_genai import ChatGoogleGenerativeAI
@@ -8,11 +9,15 @@ from dotenv import load_dotenv
 load_dotenv()
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=os.getenv("GEMINI_API_KEY"))
 
+#Add memory to store past interactions
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
 tools = [paper_search_tool, summarize_tool, gap_analysis_tool, methodology_tool]
 
 agent = initialize_agent(
     tools=tools,
     llm=llm,
-    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    agent="chat-conversational-react-description",
+    memory=memory,
     verbose=True
 )
